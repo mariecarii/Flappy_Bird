f3c721cf996c1f9ae7f499490b7ed9af9f2319ca
Marie Cari <mcari@nyc.yearup.org>
2023-01-13 11:08:27 -0500

    flappy "bird" created

--- FlappyPanel.java
+++ FlappyPanel.java
@@ -8,14 +8,42 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class FlappyPanel extends JPanel {
+public class FlappyPanel extends JPanel implements KeyListener, ActionListener {
+
+    final int WIDTH = 525;
+    final int HEIGHT = 550;
+    int flappyHeight = HEIGHT / 4;
+    int velocity = 0;
 
 
     public FlappyPanel() {
 
-        setSize(525, 550);
-
+        setSize(WIDTH, HEIGHT);
+        setFocusable(true);
+        addKeyListener(this);
 
         setBackground(Color.black);
+        new Timer(40, this).start();
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawFlappy(g);
+    }
+    private void drawFlappy(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(75, flappyHeight, 50, 50);
+    }
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
+    public void keyTyped (KeyEvent e) {
+
+    }
+    public void keyPressed(KeyEvent e) {
+
+    }
+    public void keyReleased(KeyEvent e) {
+
     }
 }
